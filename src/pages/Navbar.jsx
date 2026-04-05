35b234586a96dc3f8aa22ff4a2ee4a32539488e9
cruzadriaan <adriancruzemail@gmail.com>
2026-04-05 20:09:26 +0800

    setting up and converting vanilla css to tailwind css

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -6,23 +6,24 @@ import BeatJamLogo from '../images/beatjam-logo.png';
 export const Navbar = () => {
 
     return (
-        <div className='container'>
-            <div className="logo-container">
+        <div className='flex justify-between mx-5 my-0'>
+            <div className="p-1">
                 <Link to="/"><img src={BeatJamLogo} width="140" /></Link>
 
             </div>
-            <div className="nav-links">
+
+            <div className="flex justify-right mr-1.9 items-center p-0.6  gap-8 font-semibold text-xl text-gray-800 hover:text-gray-500">
                 <Link to="/">Home</Link>
                 <Link to="/menu">Menu</Link>
                 <Link to="/gallery">Gallery</Link>
                 <Link to="/contact">Contacts</Link>
+
                 <div className="register-container">
                     <Link to="/signin" className='signin'>Sign In</Link>
                     <Link to="/signup" className='signup'>Sign Up</Link>
                 </div>
 
             </div>
-
         </div>
 
     );
