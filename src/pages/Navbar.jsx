c3420c90898aba4bfcd923aeabe77b368b229e31
cruzadriaan <adriancruzemail@gmail.com>
2026-04-10 15:25:18 +0800

    adjusted sidebar closing animation

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -38,14 +38,14 @@ export const Navbar = () => {
 
                 </div>
 
-                <div className={`${isOpen ? "translate-x-0 opacity-100 shadow-xl " : "translate-x-full opacity-0 sm:flex sm:translate-x-5 sm:opacity-100 "} 
+                <div className={`${isOpen ? "translate-x-0  shadow-xl " : "translate-x-full sm:flex sm:translate-x-5 sm:opacity-100 "} 
                 text-gray-800 flex flex-col items-left fixed top-0 right-0 z-50 p-5 h-dvh gap-10
-                sm:flex-row sm:relative sm:flex sm:top-0 sm:h-0  transition-all duration-500`}>
+                sm:flex-row sm:relative sm:flex sm:top-0 sm:h-0  transition-transform transition-opacity duration-500`}>
 
-                    <div className='flex justify-end'>
+                    <div className='flex flex-col items-end'>
                         <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>
                             {isOpen &&
-                                <div className='flex flex-col justify-center items-center sm:hidden border rounded-full p-1 px-3 font-semibold hover:text-white text-lg border-black hover:bg-gray-800'>
+                                <div className=' w-10 sm:hidden border rounded-full p-1 px-3 font-semibold hover:text-white text-lg border-black hover:bg-gray-800'>
                                     X
                                 </div>
                             }
@@ -54,8 +54,8 @@ export const Navbar = () => {
 
                     {Navlinks.map((nav, index) => (
                         <div key={index} className='flex gap-3 w-[150px] p-1'>
-                            {isOpen && <FontAwesomeIcon icon={nav.icon} className="text-gray-700 h-5 " />}
-                            <Link to={nav.src} className='pb-0.2 font-bold border-b-2 border-transparent transition-all hover:border-[#E59A18]' onClick={() => setIsOpen(false)}>{nav.name}</Link>
+                            <FontAwesomeIcon icon={nav.icon} className="text-gray-700 h-5 " />
+                            <Link to={nav.src} className='pb-0.2 font-bold border-b-2 border-transparent  hover:border-[#E59A18]' onClick={() => setIsOpen(false)}>{nav.name}</Link>
                         </div>
                     ))}
 
