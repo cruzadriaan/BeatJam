78c7e688d3886a8148ad67b96596076e0f72ade3
cruzadriaan <adriancruzemail@gmail.com>
2026-04-11 17:29:06 +0800

    adding preview image in the gallery when clicked

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,22 +1,36 @@
 import './Gallery.css';
 import {gallery, events} from './GalleryData.js';
+import { useState } from 'react';
 
 export const Gallery = () => {
+    const [openImage, setOpenImage] = useState(false);
+
     return (
         <div className='flex flex-col justify-center items-center mt-7'>  
             <h1 className=' bg-yellow-600 font-bold text-white w-[190px] rounded-lg'>Satisfied Customers</h1>
             <div className="grid grid-cols-2 gap-2 m-2">
                 {gallery.map((gal, index) => (
-                    <img className='max-w-full' key={index} src={gal.image} alt={gal.name} />
+                    <img className='max-w-full' key={index} src={gal.image} alt={gal.name} onClick={() => setOpenImage(gal)} />
                 ))}
             </div>
 
             <h1 className='mt-4 bg-yellow-600 font-bold text-white w-[190px] rounded-lg'>Events</h1>
             <div className="grid grid-cols-2 gap-2 m-2">
                 {events.map((event) => (
-                    <img className='max-w-full' key={event.name} src={event.image} alt={event.name} />
+                    <img className='max-w-full' key={event.name} src={event.image} alt={event.name} onClick={() => setOpenImage(event)} />
                 ))}
             </div>
+
+            {openImage &&
+            (
+            <div className='bg-black bg-opacity-85 fixed inset-0 flex flex-col justify-center item-center' onClick={()=> setOpenImage(null)}>
+                    <div className='bg-white p-4'>
+                        <img src={openImage.image} alt="" />
+                    </div>
+            </div>
+            )}
+
+
         </div>
 
     );
