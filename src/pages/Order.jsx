91f9bc1c2ca287cd6499849f38702c0c2c1e4a03
cruzadriaan <adriancruzemail@gmail.com>
2026-04-11 14:35:06 +0800

    Added a state/setter in order form combined inside the formDate state, new learnings

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,7 +1,34 @@
 import './Order.css';
 import Cat from '../images/copper.png';
+import { useState } from 'react';
 
 export const Order = () => {
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        contact: "",
+        email: "",
+        deliveryAddress: "",
+        notes: ""
+    });
+
+    const handleInput = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value });
+    }
+
+    const handleClear = () => {
+        setFormData({
+            firstName: "",
+            lastName: "",
+            contact: "",
+            email: "",
+            deliveryAddress: "",
+            notes: ""
+        })
+    }
+
     return (
         <>
             <div className='flex flex-col gap-5 p-[20px] m-10 border border-gray-300  rounded-xl h-fit max-w-[90%] w-fit drop-shadow-sm items-center mx-auto'>
@@ -12,19 +39,27 @@ export const Order = () => {
                     <div className='flex items-left gap-5'>
                         <label className='text-sm text-left font-semibold text-gray-700'>
                             First Name:
-                            <input type="text" className='border border-gray-400 p-1 w-full rounded-md' required />
+                            <input type="text" className='border border-gray-400 p-1 w-full rounded-md' name="firstName" value={formData.firstName} onChange={handleInput} required />
                         </label>
 
                         <label className='text-sm text-left font-semibold text-gray-700'>
                             Last Name:
-                            <input type="text" className='border border-gray-400  p-1 w-full rounded-md' required />
+                            <input type="text" className='border border-gray-400  p-1 w-full rounded-md' value={formData.lastName} onChange={handleInput} required />
+                        </label>
+                    </div>
+
+
+                    <div className='flex flex-row gap-10'>
+                        <label className='text-sm text-left font-semibold  text-gray-700 '>
+                            Contact Number:
+                            <input type="email" placeholder="09xx-xxx-xxxx" className='border border-gray-400  p-1 ml-1 w-full rounded-md' value={formData.contact} onChange={handleInput} />
                         </label>
                     </div>
 
                     <div className='flex flex-row gap-10'>
                         <label className='text-sm text-left font-semibold  text-gray-700 '>
                             Email Address:
-                            <input type="email" placeholder="(optional)" className='border border-gray-400  p-1 ml-1 w-full rounded-md' />
+                            <input type="email" placeholder="(optional)" className='border border-gray-400  p-1 ml-1 w-full rounded-md' value={formData.email} onChange={handleInput} />
                         </label>
                     </div>
 
@@ -33,19 +68,19 @@ export const Order = () => {
                             <label className='flex items-left justify-left text-sm font-semibold text-gray-700 '>
                                 Delivery Address:
                             </label>
-                            <textarea type="text" className='border border-gray-400  p-1 w-full  rounded-md' required />
+                            <textarea type="text" className='border border-gray-400  p-1 w-full  rounded-md resize-none' value={formData.deliveryAddress} onChange={handleInput} required />
                         </div>
 
                         <div className='flex flex-col '>
                             <label className='flex items-left justify-left text-sm font-semibold text-gray-700 '>
                                 Notes:
                             </label>
-                            <textarea type="text" className='border border-gray-400  p-1 w-full rounded-md' required />
+                            <textarea type="text" className='border border-gray-400  p-1 w-full rounded-md resize-none' value={formData.notes} onChange={handleInput} required />
                         </div>
                     </div>
                     <div className='flex flex-row justify-center items-center gap-5'>
                         <button className='p-3 border w-[140px]  rounded-lg bg-yellow-500 text-white font-bold'>Submit Order</button>
-                        <button className='p-3 border w-[140px] rounded-lg bg-red-500 text-white font-bold'>Clear</button>
+                        <button className='p-3 border w-[140px] rounded-lg bg-red-500 text-white font-bold' onClick={handleClear}>Clear</button>
                     </div>
                 </form>
                 <div className='flex flex-col justify-center items-center h-dv sm:mt-30 mt-[100px]'>
