80f662ad49ed8a058f5e513f034d6708a9bb061c
cruzadriaan <adriancruzemail@gmail.com>
2026-04-13 22:00:29 +0800

    adjusted arrow in carousel and margin in contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -8,7 +8,7 @@ export const ContactUs = () => {
 
       <div className='flex flex-col items-center justify-center text-md gap-3 '>
         <div className=''>
-          <h1>Address: Cawayan Uno Badajos, San Francisco Quezon</h1>
+          <h1>Address: Cawayan Uno Badajos, San Francisco Quezon Orange Apartment</h1>
         </div>
         <div>
           <h1>Phone number: 0999-954-8120</h1>
@@ -17,7 +17,7 @@ export const ContactUs = () => {
           <h1>Email Address: beatjamrestobar@gmail.com</h1>
         </div>
 
-        <form action="" className='flex flex-col gap-5 items-center justify-center  mt-10'>
+        <form action="" className='flex flex-col gap-5 items-center justify-center mt-10 mx-5'>
           <input type="text" placeholder='First Name' className=''/>
           <input type="email" placeholder='Email' />
           <textarea name="" id="" placeholder='Message' className='h-[200px] border resize-none'></textarea>
