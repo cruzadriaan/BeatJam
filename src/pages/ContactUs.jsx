109cfe2829a1fccc03f892f9747cdc1379d615cd
cruzadriaan <adriancruzemail@gmail.com>
2026-04-13 21:45:03 +0800

    fixed global button assignment in contact.css and replaced in tailwind css

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -8,7 +8,7 @@ export const ContactUs = () => {
 
       <div className='flex flex-col items-center justify-center text-md gap-3 '>
         <div className=''>
-          <h1>Address: Cawaian Uno Badajos, San Francisco Quezon</h1>
+          <h1>Address: Cawayan Uno Badajos, San Francisco Quezon</h1>
         </div>
         <div>
           <h1>Phone number: 0999-954-8120</h1>
@@ -20,13 +20,13 @@ export const ContactUs = () => {
           <h1>Social media:</h1>
         </div>
 
-        <form action="" className='flex flex-col gap-5 items-center justify-center text-red-900 mt-10'>
+        <form action="" className='flex flex-col gap-5 items-center justify-center  mt-10'>
           <input type="text" placeholder='First Name' className=''/>
           <input type="email" placeholder='Email' />
           <textarea name="" id="" placeholder='Message' className='h-[200px] border resize-none'></textarea>
           <div className='flex gap-5'>
-            <button className='px-5 py-1 '>Submit</button>
-            <button className='px-5 py-1'>Clear</button>
+            <button className='w-[80px] py-1 border border-gray-900'>Submit</button>
+            <button className='w-[80px] py-1 border border-gray-900'>Clear</button>
           </div>
 
         </form>
