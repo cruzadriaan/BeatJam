824fcb7caee2df9a20c0703fbc8db7bdf76ae6d9
cruzadriaan <adriancruzemail@gmail.com>
2026-04-21 23:24:57 +0800

    fixed deliveryOptions page, makes it responsive

--- src/pages/DeliveryOptions.jsx
+++ src/pages/DeliveryOptions.jsx
@@ -17,20 +17,20 @@ export const DeliveryOptions = () => {
 
     return (
         <div className='flex items-center justify-center h-screen w-screen'>
-            <div className="flex flex-col text-white gap-12 py-[80px] md:px-[120px] sm:px-[60px] px-[18px] bg-gray-800 rounded-xl">
+            <div className="flex flex-col text-white gap-12 sm:py-[80px] py-[60px] md:px-[120px] sm:px-[60px] px-[35px] bg-gray-800 rounded-xl">
                 <div className="bg-transparent">
                     <h1 className=" text-white sm:text-4xl text-3xl ">Delivery Options</h1>
                 </div>
 
                 <div className="flex sm:gap-12 gap-7 bg-transparent">
 
-                    <div className='flex flex-col items-center justify-center gap-10 cursor-pointer font-bold bg-red-500 w-[200px] sm:p-10 p-5 rounded-xl hover:opacity-90 ' onClick={handleOrderPickUp}>
-                        <img src={PickUp} className=' w-[60px] bg-transparent' alt="" />
+                    <div className='flex flex-col items-center justify-center gap-5 cursor-pointer font-bold bg-red-500 sm:w-[180px] w-[160px] sm:p-10 p-5 rounded-xl hover:opacity-90 ' onClick={handleOrderPickUp}>
+                        <img src={PickUp} className='sm:w-[60px] w-[40px] bg-transparent' alt="" />
                         <button className=''>Pick Up</button>
                     </div>
 
-                    <div className='flex flex-col items-center justify-center gap-10 cursor-pointer font-bold bg-green-500 w-[200px] sm:p-10 p-5 rounded-xl hover:opacity-90'  onClick={handleOrderDelivery}>
-                        <img src={Delivery} className=' w-[80px] bg-transparent' alt="" />
+                    <div className='flex flex-col items-center justify-center gap-5 cursor-pointer font-bold bg-green-500 sm:w-[180px] w-[160px] sm:p-10 p-5 rounded-xl hover:opacity-90'  onClick={handleOrderDelivery}>
+                        <img src={Delivery} className='sm:w-[80px] w-[60px] bg-transparent' alt="" />
                         <button className=''>Delivery</button>
                     </div>
 
