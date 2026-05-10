6687d634141947cae8459f021ae132ff4055fd54
unknown <adriancruzemail@gmail.com>
2026-05-10 15:26:42 +0800

    added sizzlingmenu items for sizzling solo

--- src/pages/MenuDetails.js
+++ src/pages/MenuDetails.js
@@ -4,6 +4,12 @@ import Sotanghon from '../images/menu/menu-item/sotanghon.png';
 import MikiBihon from '../images/menu/menu-item/MikiBihon.png';
 import BihonGuisado from '../images/menu/menu-item/bihonGuisado.png';
 import Lomi from '../images/menu/menu-item/Lomi.png';
+import PorkSisig from '../images/menu/menu-item/PorkSisig.png';
+import ChickenSisig from '../images/menu/menu-item/ChickenSisig.png';
+import BurgerSteak from '../images/menu/menu-item/BurgerSteak.png';
+import HungarianSausage from '../images/menu/menu-item/HungarianSausage.png';
+import BlackPepperChicken from '../images/menu/menu-item/BlackPepperChicken.png';
+import BlackPepperBeef from '../images/menu/menu-item/BlackPepperBeef.png';
 
 export const MenuDetails = [
     { 
@@ -34,7 +40,7 @@ export const MenuDetails = [
       img: BihonGuisado,
       name: "Bihon Guisado",
       quantity: 1,
-      description: " cooked with savory soy sauce, calamansi, vegetables, and proteins like chicken, pork, or shrimp."
+      description: " cooked with savory soy sauce, calamansi, vegetables, and proteins."
     },
     {
       img: Lomi,
@@ -42,4 +48,43 @@ export const MenuDetails = [
       quantity: 1,
       description: "fresh egg noodles served in a savory, gooey, and starch-thickened soup."
     }
+]
+
+export const SizzlingMenu = [
+  {
+      img: PorkSisig,
+      name: "PorkSisig",
+      quantity: 1,
+      description: "chopped pig parts—typically cheeks, snout, and ears—mixed with liver, onions, and chili peppers."
+  },
+  {
+      img: ChickenSisig,
+      name: "Chicken Sisig",
+      quantity: 1,
+      description: "made from chopped grilled or fried chicken, chicken liver, onions, chili peppers, and citrus."
+  },
+  {
+      img: BurgerSteak,
+      name: "Burger Steak",
+      quantity: 1,
+      description: "high-quality steak cuts (such as sirloin, ribeye, or chuck) rather than standard mixed beef trimmings."
+  },
+    {
+      img: HungarianSausage,
+      name: "Hungarian",
+      quantity: 1,
+      description: "ground pork sausage heavily seasoned with paprika, garlic, caraway seeds, and pepper."
+  },
+    {
+      img: BlackPepperChicken,
+      name: "Black Pepper Chicken",
+      quantity: 1,
+      description: "a savory Chinese-American stir-fry featuring tender, bite-sized chicken (breast or thigh)."
+  },
+    {
+      img: BlackPepperBeef,
+      name: "Black Pepper Beef",
+      quantity: 1,
+      description: "Chinese-American stir-fry featuring tenderized beef, onions, and bell peppers coated in a savory, glossy sauce."
+  },
 ]
--- src/pages/OrderSelection.jsx
+++ src/pages/OrderSelection.jsx
@@ -1,5 +1,5 @@
-import { MenuDetails } from "./MenuDetails";
-import {useState} from 'react';
+import { MenuDetails, SizzlingMenu } from "./MenuDetails";
+import { useState } from 'react';
 
 export const OrderSelection = ({ deliveryOption }) => {
 
@@ -10,8 +10,8 @@ export const OrderSelection = ({ deliveryOption }) => {
         <div className='w-full mt-20  space-y-5'>
             <h1>{deliveryOption}</h1>
 
-            <div className="bg-blue-300 mx-10 cursor-pointer text-white font-bold" 
-            onClick={()=>setIsPancitVisible(!isPancitVisible)}>Pancit</div>
+            <div className="bg-blue-300 mx-10 cursor-pointer text-white font-bold"
+                onClick={() => setIsPancitVisible(!isPancitVisible)}>Pancit</div>
 
             <div className={`${isPancitVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
@@ -35,8 +35,32 @@ export const OrderSelection = ({ deliveryOption }) => {
                     </div>
                 ))}
             </div>
-            
 
+            <div className="bg-blue-300 mx-10 cursor-pointer text-white font-bold"
+                onClick={() => setIsSizzlingVisible(!isSizzlingVisible)}>Sizzling Solo</div>
+
+            <div className={`${isSizzlingVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
+            transition-all`}>
+
+                {SizzlingMenu.map((menu, index) => (
+                    <div key={index} className='p-4 border border-gray-600 rounded-md'>
+                        <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
+
+                        <h1 className='text-md'>{menu.name}</h1>
+                        <div className='flex items-center justify-center gap-3 mt-2'>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
+                            <h1>1</h1>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>+</button>
+                        </div>
+                        <div className='mt-2 mx-5'>
+                            <p className='text-[13px] '>{menu.description}</p>
+                        </div>
+                        <div className='mt-2'>
+                            <button className='text-sm bg-blue-500 text-white py-1 px-2 rounded-md'>Add to Cart</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
         </div>
     );
