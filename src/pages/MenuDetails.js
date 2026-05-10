93884e1b659ed0d9bc2b2f4709a4387747bf1f31
unknown <adriancruzemail@gmail.com>
2026-05-10 17:52:50 +0800

    added combo meals in order selection

--- src/pages/MenuDetails.js
+++ src/pages/MenuDetails.js
@@ -10,42 +10,55 @@ import BurgerSteak from '../images/menu/menu-item/BurgerSteak.png';
 import HungarianSausage from '../images/menu/menu-item/HungarianSausage.png';
 import BlackPepperChicken from '../images/menu/menu-item/BlackPepperChicken.png';
 import BlackPepperBeef from '../images/menu/menu-item/BlackPepperBeef.png';
+import ComboA from '../images/menu/menu-item/ComboA.png';
+import ComboB from '../images/menu/menu-item/ComboB.png';
+import ComboC from '../images/menu/menu-item/ComboC.png';
+import ComboD from '../images/menu/menu-item/ComboD.png';
+import ComboE from '../images/menu/menu-item/ComboE.png';
+import ComboF from '../images/menu/menu-item/ComboF.png';
+import ComboG from '../images/menu/menu-item/ComboG.png';
 
 export const MenuDetails = [
     { 
       img: Chami,
       name: "Chami",
       quantity: 1,
+      price: 80,
       description: "sweet, spicy, and saucy stir-fried noodle dish"  
     },
     {
       img: PancitLucban,
       name: "Pancit Lucban",
       quantity: 1,
+      price: 80,
       description: "mixed with pork belly, shrimp, and vegetables"
     },
     {
       img: Sotanghon,
       name: "Sotanghon",
       quantity: 1,
+      price: 80,
       description: "stir-fried noodle dish combining thick, savory egg noodles"
     },
     {
       img: MikiBihon,
       name: "Miki Bihon",
       quantity: 1,
+      price: 80,
       description: "stir-fried with meat, vegetables, soy sauce, and broth"
     },
     {
       img: BihonGuisado,
       name: "Bihon Guisado",
       quantity: 1,
+      price: 80,
       description: " cooked with savory soy sauce, calamansi, vegetables, and proteins."
     },
     {
       img: Lomi,
       name: "Lomi",
       quantity: 1,
+      price: 80,
       description: "fresh egg noodles served in a savory, gooey, and starch-thickened soup."
     }
 ]
@@ -55,36 +68,94 @@ export const SizzlingMenu = [
       img: PorkSisig,
       name: "PorkSisig",
       quantity: 1,
+      price: 130,
       description: "chopped pig parts—typically cheeks, snout, and ears—mixed with liver, onions, and chili peppers."
   },
   {
       img: ChickenSisig,
       name: "Chicken Sisig",
       quantity: 1,
+      price: 130,
       description: "made from chopped grilled or fried chicken, chicken liver, onions, chili peppers, and citrus."
   },
   {
       img: BurgerSteak,
       name: "Burger Steak",
       quantity: 1,
+      price: 130,
       description: "high-quality steak cuts (such as sirloin, ribeye, or chuck) rather than standard mixed beef trimmings."
   },
     {
       img: HungarianSausage,
       name: "Hungarian",
       quantity: 1,
+      price: 130,
       description: "ground pork sausage heavily seasoned with paprika, garlic, caraway seeds, and pepper."
   },
     {
       img: BlackPepperChicken,
       name: "Black Pepper Chicken",
       quantity: 1,
+      price: 130,
       description: "a savory Chinese-American stir-fry featuring tender, bite-sized chicken (breast or thigh)."
   },
     {
       img: BlackPepperBeef,
       name: "Black Pepper Beef",
       quantity: 1,
+      price: 140,
       description: "Chinese-American stir-fry featuring tenderized beef, onions, and bell peppers coated in a savory, glossy sauce."
+  },
+]
+
+export const ComboMenuList = [
+  {
+      img: ComboA,
+      name: "Combo A",
+      quantity: 1,
+      price: 180,
+      description: "Burger Steak with Shanghai."
+  },
+  {
+      img: ComboB,
+      name: "Combo B",
+      quantity: 1,
+      price: 180,
+      description: "Double Burger Steak."
+  },
+  {
+      img: ComboC,
+      name: "Combo C",
+      quantity: 1,
+      price: 180,
+      description: "Chicken with Burger Steak."
+  },
+  {
+      img: ComboD,
+      name: "Combo D",
+      quantity: 1,
+      price: 180,
+      description: "Porkchop with Bone Marrow."
+  },
+  {
+      img: ComboE,
+      name: "Combo E",
+      quantity: 1,
+      price: 180,
+      description: "Sisig with Bone Marrow."
+  },
+  {
+      img: ComboF,
+      name: "Combo F",
+      quantity: 1,
+      price: 180,
+      description: "Hungarian with Sisig."
+  },
+  {
+      img: ComboG,
+      name: "Combo G",
+      quantity: 1,
+      price: 180,
+      description: "Hungarian with Burger Steak."
   },
 ]
--- src/pages/OrderSelection.jsx
+++ src/pages/OrderSelection.jsx
@@ -1,17 +1,24 @@
-import { MenuDetails, SizzlingMenu } from "./MenuDetails";
+import { MenuDetails, SizzlingMenu, ComboMenuList } from "./MenuDetails";
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 export const OrderSelection = ({ deliveryOption }) => {
 
     const [isPancitVisible, setIsPancitVisible] = useState(false);
     const [isSizzlingVisible, setIsSizzlingVisible] = useState(false);
+    const [isComboVisible, setIsComboVisible] = useState(false);
+    const [selectedSize, setSelectedSize] = useState(80);
 
     return (
         <div className='w-full mt-20  space-y-5'>
             <h1>{deliveryOption}</h1>
 
-            <div className="bg-blue-300 mx-10 cursor-pointer text-white font-bold"
-                onClick={() => setIsPancitVisible(!isPancitVisible)}>Pancit</div>
+            <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
+                onClick={() => setIsPancitVisible(!isPancitVisible)}>
+                    <h1 className="!text-white font-bold">Pancit</h1>
+                    <FontAwesomeIcon className={`${isPancitVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
+                </div>
 
             <div className={`${isPancitVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
@@ -21,6 +28,18 @@ export const OrderSelection = ({ deliveryOption }) => {
                         <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
 
                         <h1 className='text-md'>{menu.name}</h1>
+
+                        <div className="flex justify-center items-center gap-4 p-3">
+                            <h1 className="text-sm">₱{selectedSize}</h1>
+                            <select className="border border-gray-500" name="" id=""
+                            onChange={(e) => {setSelectedSize(e.target.value)}}>
+                                <option value="80">Small</option>
+                                <option value="120">Double</option>
+                                <option value="320">Sharing</option>
+                                <option value="450">Barkada</option>
+                            </select>
+                        </div>
+                        
                         <div className='flex items-center justify-center gap-3 mt-2'>
                             <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
                             <h1>1</h1>
@@ -36,8 +55,11 @@ export const OrderSelection = ({ deliveryOption }) => {
                 ))}
             </div>
 
-            <div className="bg-blue-300 mx-10 cursor-pointer text-white font-bold"
-                onClick={() => setIsSizzlingVisible(!isSizzlingVisible)}>Sizzling Solo</div>
+            <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
+                onClick={() => setIsSizzlingVisible(!isSizzlingVisible)}>
+                    <h1 className="!text-white font-bold">Sizzling Solo</h1>
+                    <FontAwesomeIcon className={`${isSizzlingVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
+                </div>
 
             <div className={`${isSizzlingVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
@@ -61,6 +83,36 @@ export const OrderSelection = ({ deliveryOption }) => {
                     </div>
                 ))}
             </div>
+
+            <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
+                onClick={() => setIsComboVisible(!isComboVisible)}>
+                    <h1 className="!text-white font-bold">Combo Meals</h1>
+                    <FontAwesomeIcon className={`${isComboVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
+                </div>
+
+            <div className={`${isComboVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
+            transition-all`}>
+
+                {ComboMenuList.map((menu, index) => (
+                    <div key={index} className='p-4 border border-gray-600 rounded-md'>
+                        <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
+
+                        <h1 className='text-md'>{menu.name}</h1>
+                        <div className='flex items-center justify-center gap-3 mt-2'>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
+                            <h1>1</h1>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>+</button>
+                        </div>
+                        <div className='mt-2 mx-5'>
+                            <p className='text-[13px] '>{menu.description}</p>
+                        </div>
+                        <div className='mt-2'>
+                            <button className='text-sm bg-blue-500 text-white py-1 px-2 rounded-md'>Add to Cart</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
 
         </div>
     );
