fd33a601aebeab57643e4149688163f23c059744
unknown <adriancruzemail@gmail.com>
2026-05-10 22:32:46 +0800

    added a flavored chicken in orderSelection, to be continued

--- src/pages/OrderSelection.jsx
+++ src/pages/OrderSelection.jsx
@@ -1,25 +1,38 @@
 import { MenuDetails, SizzlingMenu, ComboMenuList } from "./MenuDetails";
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 export const OrderSelection = ({ deliveryOption }) => {
 
     const [isPancitVisible, setIsPancitVisible] = useState(false);
-    const [isSizzlingVisible, setIsSizzlingVisible] = useState(false);
-    const [isComboVisible, setIsComboVisible] = useState(false);
+    const [isSizzlingSoloVisible, setIsSizzlingSoloVisible] = useState(false);
+    const [isSizzlerComboVisible, setIsSizzlerComboVisible] = useState(false);
+    const [isFChickenVisible, setisFChickenVisible] = useState(false);
     const [selectedSize, setSelectedSize] = useState(80);
 
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate("../deliveryOptions")
+    }
+
     return (
+
         <div className='w-full mt-20  space-y-5'>
             <h1>{deliveryOption}</h1>
 
             <h1 className="text-left ml-10">Step 1: Select your order</h1>
+
+            {/* Menu items */}
+
+            {/* Pancit */}
             <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
                 onClick={() => setIsPancitVisible(!isPancitVisible)}>
-                    <h1 className="!text-white font-bold">Pancit</h1>
-                    <FontAwesomeIcon className={`${isPancitVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
-                </div>
+                <h1 className="!text-white font-bold">Pancit</h1>
+                <FontAwesomeIcon className={`${isPancitVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight} />
+            </div>
 
             <div className={`${isPancitVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
@@ -33,14 +46,14 @@ export const OrderSelection = ({ deliveryOption }) => {
                         <div className="flex justify-center items-center gap-4 p-3">
                             <h1 className="text-sm">₱{selectedSize}</h1>
                             <select className="border border-gray-500" name="" id=""
-                            onChange={(e) => {setSelectedSize(e.target.value)}}>
+                                onChange={(e) => { setSelectedSize(e.target.value) }}>
                                 <option value="80">Small</option>
                                 <option value="120">Double</option>
                                 <option value="320">Sharing</option>
                                 <option value="450">Barkada</option>
                             </select>
                         </div>
-                        
+
                         <div className='flex items-center justify-center gap-3 mt-2'>
                             <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
                             <h1>1</h1>
@@ -56,13 +69,14 @@ export const OrderSelection = ({ deliveryOption }) => {
                 ))}
             </div>
 
+            {/* Sizzler Solo */}
             <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
-                onClick={() => setIsSizzlingVisible(!isSizzlingVisible)}>
-                    <h1 className="!text-white font-bold">Sizzling Solo</h1>
-                    <FontAwesomeIcon className={`${isSizzlingVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
-                </div>
+                onClick={() => setIsSizzlingSoloVisible(!isSizzlingSoloVisible)}>
+                <h1 className="!text-white font-bold">Sizzler Solo</h1>
+                <FontAwesomeIcon className={`${isSizzlingSoloVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight} />
+            </div>
 
-            <div className={`${isSizzlingVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
+            <div className={`${isSizzlingSoloVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
 
                 {SizzlingMenu.map((menu, index) => (
@@ -85,13 +99,14 @@ export const OrderSelection = ({ deliveryOption }) => {
                 ))}
             </div>
 
+            {/* Sizzler Combo Meals */}
             <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
-                onClick={() => setIsComboVisible(!isComboVisible)}>
-                    <h1 className="!text-white font-bold">Combo Meals</h1>
-                    <FontAwesomeIcon className={`${isComboVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight}/>
-                </div>
+                onClick={() => setIsSizzlerComboVisible(!isSizzlerComboVisible)}>
+                <h1 className="!text-white font-bold">Sizzler Combo Meals</h1>
+                <FontAwesomeIcon className={`${isSizzlerComboVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight} />
+            </div>
 
-            <div className={`${isComboVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
+            <div className={`${isSizzlerComboVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
             transition-all`}>
 
                 {ComboMenuList.map((menu, index) => (
@@ -114,8 +129,43 @@ export const OrderSelection = ({ deliveryOption }) => {
                 ))}
             </div>
 
-            <div className="flex justify-center gap-5">
-                <button className="bg-gray-400 px-3 py-1 cursor-pointer hover:opacity-85">Back</button>
+            {/* Flavored Chicken */}
+            <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
+                onClick={() => setisFChickenVisible(!isFChickenVisible)}>
+                <h1 className="!text-white font-bold">Flavored Chicken</h1>
+                <FontAwesomeIcon className={`${isFChickenVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight} />
+            </div>
+
+            <div className={`${isFChickenVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 flex flex-col justify-center mx-5 
+            transition-all`}>
+
+                <div className="flex flex-col justify-center items-center">
+                    <label className="" htmlFor="">
+                        <input type="radio" name="flavor" value="sweetChili" />
+                        <span>Sweet Chili </span>
+                    </label>
+                    <label htmlFor=""><input type="radio" name="flavor" value="honeyGlazed"/>Honey Glazed</label>
+                    <label htmlFor=""><input type="radio" name="flavor" value="spicyGarlicMayo"/>Spicy Garlic Mayo</label>
+                    <label htmlFor=""><input type="radio" name="flavor" value="arabKebab"/>Arabian's Kebab</label>
+                    <label htmlFor=""><input type="radio" name="flavor" value="fireballBuffalo"/>Fireball Buffalo</label>
+                </div>
+                
+                <div className='flex items-center justify-center gap-3 mt-2'>
+                    <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
+                    <h1>1</h1>
+                    <button className='h-6 w-6 rounded-full border border-gray-500'>+</button>
+                </div>
+
+                <div className='mt-2'>
+                    <button className='text-sm bg-blue-500 text-white py-1 px-2 rounded-md'>Add to Cart</button>
+                </div>
+            </div>
+
+
+            {/* Buttom buttons */}
+            <div className="flex justify-center gap-5 py-10">
+                <button className="bg-gray-400 px-3 py-1 cursor-pointer hover:opacity-85"
+                    onClick={handleBack}>Back</button>
                 <button className="bg-green-500 px-3 py-1 cursor-pointer hover:opacity-85">Next</button>
             </div>
         </div>
