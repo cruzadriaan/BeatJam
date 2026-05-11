289832771d19eedb6abf05111e8b5ef6ae691b0c
unknown <adriancruzemail@gmail.com>
2026-05-11 08:45:08 +0800

    added silog meals in orderSelection menu

--- src/pages/MenuDetails.js
+++ src/pages/MenuDetails.js
@@ -17,145 +17,229 @@ import ComboD from '../images/menu/menu-item/ComboD.png';
 import ComboE from '../images/menu/menu-item/ComboE.png';
 import ComboF from '../images/menu/menu-item/ComboF.png';
 import ComboG from '../images/menu/menu-item/ComboG.png';
+import HotSilog from '../images/menu/menu-item/hotSilog.png';
+import Tocilog from '../images/menu/menu-item/toSilog.png';
+import BacSilog from '../images/menu/menu-item/bacSilog.png';
+import Sausilog from '../images/menu/menu-item/sauSilog.png';
+import Tapsilog from '../images/menu/menu-item/tapSilog.png';
+import CornSilog from '../images/menu/menu-item/cornSilog.png';
+import BangSilog from '../images/menu/menu-item/bangSilog.png';
+import PorkSilog from '../images/menu/menu-item/porkSilog.png';
+import SpamSilog from '../images/menu/menu-item/spamSilog.png';
+import ChicSilog from '../images/menu/menu-item/ChicSilog.png';
 
 export const MenuDetails = [
-    { 
-      img: Chami,
-      name: "Chami",
-      quantity: 1,
-      price: 80,
-      description: "sweet, spicy, and saucy stir-fried noodle dish"  
-    },
-    {
-      img: PancitLucban,
-      name: "Pancit Lucban",
-      quantity: 1,
-      price: 80,
-      description: "mixed with pork belly, shrimp, and vegetables"
-    },
-    {
-      img: Sotanghon,
-      name: "Sotanghon",
-      quantity: 1,
-      price: 80,
-      description: "stir-fried noodle dish combining thick, savory egg noodles"
-    },
-    {
-      img: MikiBihon,
-      name: "Miki Bihon",
-      quantity: 1,
-      price: 80,
-      description: "stir-fried with meat, vegetables, soy sauce, and broth"
-    },
-    {
-      img: BihonGuisado,
-      name: "Bihon Guisado",
-      quantity: 1,
-      price: 80,
-      description: " cooked with savory soy sauce, calamansi, vegetables, and proteins."
-    },
-    {
-      img: Lomi,
-      name: "Lomi",
-      quantity: 1,
-      price: 80,
-      description: "fresh egg noodles served in a savory, gooey, and starch-thickened soup."
-    }
+  {
+    img: Chami,
+    name: "Chami",
+    quantity: 1,
+    price: 80,
+    description: "sweet, spicy, and saucy stir-fried noodle dish"
+  },
+  {
+    img: PancitLucban,
+    name: "Pancit Lucban",
+    quantity: 1,
+    price: 80,
+    description: "mixed with pork belly, shrimp, and vegetables"
+  },
+  {
+    img: Sotanghon,
+    name: "Sotanghon",
+    quantity: 1,
+    price: 80,
+    description: "stir-fried noodle dish combining thick, savory egg noodles"
+  },
+  {
+    img: MikiBihon,
+    name: "Miki Bihon",
+    quantity: 1,
+    price: 80,
+    description: "stir-fried with meat, vegetables, soy sauce, and broth"
+  },
+  {
+    img: BihonGuisado,
+    name: "Bihon Guisado",
+    quantity: 1,
+    price: 80,
+    description: " cooked with savory soy sauce, calamansi, vegetables, and proteins."
+  },
+  {
+    img: Lomi,
+    name: "Lomi",
+    quantity: 1,
+    price: 80,
+    description: "fresh egg noodles served in a savory, gooey, and starch-thickened soup."
+  }
 ]
 
 export const SizzlingMenu = [
   {
-      img: PorkSisig,
-      name: "PorkSisig",
-      quantity: 1,
-      price: 130,
-      description: "chopped pig parts—typically cheeks, snout, and ears—mixed with liver, onions, and chili peppers."
+    img: PorkSisig,
+    name: "PorkSisig",
+    quantity: 1,
+    price: 130,
+    description: "chopped pig parts—typically cheeks, snout, and ears—mixed with liver, onions, and chili peppers."
   },
   {
-      img: ChickenSisig,
-      name: "Chicken Sisig",
-      quantity: 1,
-      price: 130,
-      description: "made from chopped grilled or fried chicken, chicken liver, onions, chili peppers, and citrus."
+    img: ChickenSisig,
+    name: "Chicken Sisig",
+    quantity: 1,
+    price: 130,
+    description: "made from chopped grilled or fried chicken, chicken liver, onions, chili peppers, and citrus."
   },
   {
-      img: BurgerSteak,
-      name: "Burger Steak",
-      quantity: 1,
-      price: 130,
-      description: "high-quality steak cuts (such as sirloin, ribeye, or chuck) rather than standard mixed beef trimmings."
+    img: BurgerSteak,
+    name: "Burger Steak",
+    quantity: 1,
+    price: 130,
+    description: "high-quality steak cuts (such as sirloin, ribeye, or chuck) rather than standard mixed beef trimmings."
   },
-    {
-      img: HungarianSausage,
-      name: "Hungarian",
-      quantity: 1,
-      price: 130,
-      description: "ground pork sausage heavily seasoned with paprika, garlic, caraway seeds, and pepper."
+  {
+    img: HungarianSausage,
+    name: "Hungarian",
+    quantity: 1,
+    price: 130,
+    description: "ground pork sausage heavily seasoned with paprika, garlic, caraway seeds, and pepper."
   },
-    {
-      img: BlackPepperChicken,
-      name: "Black Pepper Chicken",
-      quantity: 1,
-      price: 130,
-      description: "a savory Chinese-American stir-fry featuring tender, bite-sized chicken (breast or thigh)."
+  {
+    img: BlackPepperChicken,
+    name: "Black Pepper Chicken",
+    quantity: 1,
+    price: 130,
+    description: "a savory Chinese-American stir-fry featuring tender, bite-sized chicken (breast or thigh)."
   },
-    {
-      img: BlackPepperBeef,
-      name: "Black Pepper Beef",
-      quantity: 1,
-      price: 140,
-      description: "Chinese-American stir-fry featuring tenderized beef, onions, and bell peppers coated in a savory, glossy sauce."
+  {
+    img: BlackPepperBeef,
+    name: "Black Pepper Beef",
+    quantity: 1,
+    price: 140,
+    description: "Chinese-American stir-fry featuring tenderized beef, onions, and bell peppers coated in a savory, glossy sauce."
   },
 ]
 
 export const ComboMenuList = [
   {
-      img: ComboA,
-      name: "Combo A",
-      quantity: 1,
-      price: 180,
-      description: "Burger Steak with Shanghai."
+    img: ComboA,
+    name: "Combo A",
+    quantity: 1,
+    price: 180,
+    description: "Burger Steak with Shanghai"
   },
   {
-      img: ComboB,
-      name: "Combo B",
-      quantity: 1,
-      price: 180,
-      description: "Double Burger Steak."
+    img: ComboB,
+    name: "Combo B",
+    quantity: 1,
+    price: 180,
+    description: "Double Burger Steak"
   },
   {
-      img: ComboC,
-      name: "Combo C",
-      quantity: 1,
-      price: 180,
-      description: "Chicken with Burger Steak."
+    img: ComboC,
+    name: "Combo C",
+    quantity: 1,
+    price: 180,
+    description: "Chicken with Burger Steak"
   },
   {
-      img: ComboD,
-      name: "Combo D",
-      quantity: 1,
-      price: 180,
-      description: "Porkchop with Bone Marrow."
+    img: ComboD,
+    name: "Combo D",
+    quantity: 1,
+    price: 180,
+    description: "Porkchop with Bone Marrow"
   },
   {
-      img: ComboE,
-      name: "Combo E",
-      quantity: 1,
-      price: 180,
-      description: "Sisig with Bone Marrow."
+    img: ComboE,
+    name: "Combo E",
+    quantity: 1,
+    price: 180,
+    description: "Sisig with Bone Marrow"
   },
   {
-      img: ComboF,
-      name: "Combo F",
-      quantity: 1,
-      price: 180,
-      description: "Hungarian with Sisig."
+    img: ComboF,
+    name: "Combo F",
+    quantity: 1,
+    price: 180,
+    description: "Hungarian with Sisig"
   },
   {
-      img: ComboG,
-      name: "Combo G",
-      quantity: 1,
-      price: 180,
-      description: "Hungarian with Burger Steak."
+    img: ComboG,
+    name: "Combo G",
+    quantity: 1,
+    price: 180,
+    description: "Hungarian with Burger Steak"
   },
+]
+
+export const SilogMeals = [
+  {
+    img: HotSilog,
+    name: "Hotsilog",
+    quantity: 1,
+    price: 180,
+    description: "Hotdog with egg"
+  },
+  {
+    img: Tocilog,
+    name: "Tocilog",
+    quantity: 1,
+    price: 180,
+    description: "Tocino with egg"
+  },
+  {
+    img: BacSilog,
+    name: "Bacsilog",
+    quantity: 1,
+    price: 180,
+    description: "Bacon with egg"
+  },
+  {
+    img: Sausilog,
+    name: "Sausilog",
+    quantity: 1,
+    price: 180,
+    description: "Sausage with egg"
+  },
+  {
+    img: Tapsilog,
+    name: "Tapsilog",
+    quantity: 1,
+    price: 180,
+    description: "Tapa with egg"
+  },
+  {
+    img: CornSilog,
+    name: "Cornsilog",
+    quantity: 1,
+    price: 180,
+    description: "Corned beef with egg"
+  },
+  {
+    img: BangSilog,
+    name: "Bangsilog",
+    quantity: 1,
+    price: 180,
+    description: "Bangus with egg"
+  },
+  {
+    img: PorkSilog,
+    name: "Porksilog",
+    quantity: 1,
+    price: 180,
+    description: "Pork with egg"
+  },
+  {
+    img: SpamSilog,
+    name: "Spamsilog",
+    quantity: 1,
+    price: 180,
+    description: "Spam with egg"
+  },
+  {
+    img: ChicSilog,
+    name: "Chicsilog",
+    quantity: 1,
+    price: 180,
+    description: "Chicken with egg"
+  },
+
 ]
--- src/pages/OrderSelection.jsx
+++ src/pages/OrderSelection.jsx
@@ -1,4 +1,4 @@
-import { MenuDetails, SizzlingMenu, ComboMenuList } from "./MenuDetails";
+import { MenuDetails, SizzlingMenu, ComboMenuList, SilogMeals } from "./MenuDetails";
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,6 +10,7 @@ export const OrderSelection = ({ deliveryOption }) => {
     const [isSizzlingSoloVisible, setIsSizzlingSoloVisible] = useState(false);
     const [isSizzlerComboVisible, setIsSizzlerComboVisible] = useState(false);
     const [isFChickenVisible, setisFChickenVisible] = useState(false);
+    const [isSilogVisible, setIsSilogVisible] = useState(false);
     const [selectedSize, setSelectedSize] = useState(80);
 
     const navigate = useNavigate();
@@ -84,6 +85,7 @@ export const OrderSelection = ({ deliveryOption }) => {
                         <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
 
                         <h1 className='text-md'>{menu.name}</h1>
+                        <h1>₱{menu.price}</h1>
                         <div className='flex items-center justify-center gap-3 mt-2'>
                             <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
                             <h1>1</h1>
@@ -114,6 +116,7 @@ export const OrderSelection = ({ deliveryOption }) => {
                         <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
 
                         <h1 className='text-md'>{menu.name}</h1>
+                        <h1>₱{menu.price}</h1>
                         <div className='flex items-center justify-center gap-3 mt-2'>
                             <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
                             <h1>1</h1>
@@ -199,6 +202,36 @@ export const OrderSelection = ({ deliveryOption }) => {
                 </div>
             </div>
 
+            {/* Silog Meals */}
+            <div className="flex justify-center items-center bg-blue-300 mx-10 cursor-pointer"
+                onClick={() => setIsSilogVisible(!isSilogVisible)}>
+                <h1 className="!text-white font-bold">Silog Meals</h1>
+                <FontAwesomeIcon className={`${isSilogVisible ? "rotate-90" : "rotate-0"} bg-transparent ml-2 !text-white text-sm duration-200`} icon={faAngleRight} />
+            </div>
+
+            <div className={`${isSilogVisible ? "max-h-[2000px]" : "max-h-0"} overflow-hidden duration-1000 grid grid-cols-2 gap-5 mx-5 
+            transition-all`}>
+
+                {SilogMeals.map((menu, index) => (
+                    <div key={index} className='p-4 border border-gray-600 rounded-md'>
+                        <img src={menu.img} alt="" className='flex mx-auto w-[150px]' />
+
+                        <h1 className='text-md'>{menu.name}</h1>
+                        <h1>₱{menu.price}</h1>
+                        <div className='flex items-center justify-center gap-3 mt-2'>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>-</button>
+                            <h1>1</h1>
+                            <button className='h-6 w-6 rounded-full border border-gray-500'>+</button>
+                        </div>
+                        <div className='mt-2 mx-5'>
+                            <p className='text-[13px] '>{menu.description}</p>
+                        </div>
+                        <div className='mt-2'>
+                            <button className='text-sm bg-blue-500 text-white py-1 px-2 rounded-md'>Add to Cart</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
             {/* Buttom buttons */}
             <div className="flex justify-center gap-5 py-10">
